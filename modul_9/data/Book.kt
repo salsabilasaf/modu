69ea62dfadb15b0b0a1cccb3480348d12a43f225
salsabilasaf <salsabilazsftr@gmail.com>
2026-05-25 17:48:07 +0700

    Delete modul_9 directory

--- modul_9/data/Book.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pam.modul_9.data
-
-data class Book(
-    val id: Int?,
-    val judul: String,
-    val isbn: String
-)
--- modul_9/screen/UIState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pam.modul_9.screen
-
-sealed interface UIState<out T> {
-    data class Success<T>(val data: T) : UIState<T>
-    data class Error(val message: String) : UIState<Nothing>
-    object Loading : UIState<Nothing>
-}
